03897d945635bfa90840d78d5b20025af4eea540
BheshRajNeupane <108607897+BheshRajNeupane@users.noreply.github.com>
2024-01-03 15:58:09 +0545

    Transaction

--- src/items/items.service.ts
+++ src/items/items.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { CreateItemDto } from './dto/create-item.dto';
 import { UpdateItemDto } from './dto/update-item.dto';
-import { EntityManager} from 'typeorm';
+import { EntityManager } from 'typeorm';
 import { Item } from './entities/item.entity';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
@@ -10,60 +10,71 @@ import { CreateCommentDto } from './dto/create-comment.dto';
 import { Comment } from './entities/comment.entity';
 import { Tag } from './entities/tags.entity';
 
-
 @Injectable()
 export class ItemsService {
-    constructor(
-       @InjectRepository(Item) private readonly itemsRepository:Repository<Item>,
-       private readonly entityManager : EntityManager,
-       
-       ){}
+  constructor(
+    @InjectRepository(Item) private readonly itemsRepository: Repository<Item>,
+    private readonly entityManager: EntityManager,
+  ) {}
 
-   async create(createItemDto: CreateItemDto) {
-     const listing =  new Listing({
-       ...createItemDto.listing , rating:0
-      });
- 
-    const tags = createItemDto.tags.map((tag)=> new Tag(tag))
+  async create(createItemDto: CreateItemDto) {
+    const listing = new Listing({
+      ...createItemDto.listing,
+      rating: 0,
+    });
+
+    const tags = createItemDto.tags.map((tag) => new Tag(tag));
 
     const item = new Item({
       ...createItemDto,
-        comments:[],
-         tags,
-          listing
+      comments: [],
+      tags,
+      listing,
     });
-     return  this.entityManager.save(item);
+    return this.entityManager.save(item);
   }
 
-   async findAll() {
-    return this.itemsRepository.find() ;
+  async findAll() {
+    return this.itemsRepository.find();
   }
 
   findOne(id: number) {
     return this.itemsRepository.findOne({
-      where:{id} , 
-      relations:{ listing : true  , comments: true , tags:true},
+      where: { id },
+      relations: { listing: true, comments: true, tags: true },
     });
   }
 
- async update(id: number, updateItemDto: UpdateItemDto) {
-     const item = await this.itemsRepository.findOneBy({id});
-     item.public = updateItemDto.public;
-     
-    //   const comments = updateItemDto.comments.map((w)=> new Comment(w))
-    //  item.comments = comments;
-    if (updateItemDto.comments) {
-      const comments  = updateItemDto.comments.map((c) => new Comment(c));
-      // console.log(updateItemDto.comments);
-    
-    item.comments = comments;
-     }
-  
-      return await this.entityManager.save(item)
-  }
- 
+  async update(id: number, updateItemDto: UpdateItemDto) {
+    // const item = await this.itemsRepository.findOneBy({ id });
+    // item.public = updateItemDto.public;
 
-async remove(id: number) {
-     await this.itemsRepository.delete(id);
+    // if (updateItemDto.comments) {
+    //   const comments = updateItemDto.comments.map((c) => new Comment(c));
+    //   item.comments = comments;
+    // }
+
+    // return await this.entityManager.save(item);
+
+    await this.enityManager.transaction(async (entityManager) => {
+       const item = await this.itemsRepository.findOneBy({id});
+       item.public = updateItemDto.public;
+      if (updateItemDto.comments) {
+        const comments  = updateItemDto.comments.map((c) => new Comment(c));
+      item.comments = comments;
+       }
+        return await this.entityManager.save(item)
+        throw new Error() // to  testing transection
+          
+        const tagContent = `${Math.random()}`;
+        const tag = new Tag({content : tagContent })
+        await entityManager.save(tag)
+    });
+  }
+
+
+
+  async remove(id: number) {
+    await this.itemsRepository.delete(id);
   }
 }
